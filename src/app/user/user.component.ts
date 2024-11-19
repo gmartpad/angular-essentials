793030dfceb5ab8c336f62fc24e093b732aeb1b1
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2024-11-19 08:18:14 -0300

    [NG] INTRODUCING_SIGNALS - feat: implementing signals in the state management of the user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -12,15 +12,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.less'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]
+  selectedUser = signal(DUMMY_USERS[randomIndex])
+  imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar)
 
-  get imagePath() {
-    return '/assets/users/' + this.selectedUser.avatar
-  }
+  // get imagePath() {
+  //   return '/assets/users/' + this.selectedUser().avatar
+  // }
 
   onSelectUser() {
     const newRandomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
-    this.selectedUser = DUMMY_USERS[newRandomIndex]
+    // this.selectedUser = DUMMY_USERS[newRandomIndex]
+    this.selectedUser.set(DUMMY_USERS[newRandomIndex])
   }
 }
