71a0a8ff9122e256f72e64cf8b7565e39d834807
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2024-12-05 00:59:55 -0300

    [NG] MIGRATING_ALL_COMPONENTS_TO_USE_MODULES - refactor: turning all into standalone:false and importing them into the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { CardComponent } from './shared/card/card.component';
+import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [
+        AppComponent, 
+        HeaderComponent, 
+        UserComponent, 
+        TasksComponent, 
+        CardComponent, 
+        TaskComponent, 
+        NewTaskComponent
+    ],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, RouterOutlet, HeaderComponent, UserComponent, TasksComponent,]
+    imports: [BrowserModule, RouterOutlet, FormsModule]
 })
 
 export class AppModule {}
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { TaskComponent } from "../task/task.component";
-import { type Task } from '../task/task.model';
 import { type User } from '../user/user.model';
-import { NewTaskComponent } from './new-task/new-task.component';
-import { type NewTaskData } from './new-task/new-task.model';
 import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
-  standalone: true,
-  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.less'
 })
